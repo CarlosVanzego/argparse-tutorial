e5dc6c2d1fad27a65b107d73fd797e96b95eea4d
Your Name <carlosvanzego96@gmail.com>
2025-04-12 20:22:38 -0500

    positional-arguments.py practice and update.

--- positional-arguments.py
+++ positional-arguments.py
@@ -1,5 +1,22 @@
+# import argparse
+# parser = argparse.ArgumentParser()
+# parser.add_argument("echo")
+# args = parser.parse_args()
+# print(args.echo)
+
+
+# making "echo" more useful
+# import argparse
+# parser = argparse.ArgumentParser()
+# parser.add_argument("echo", help="echo the stirng you use here")
+# args = parser.parse_args()
+# print(args.echo)
+
+
+# making "echo" even MORE useful
 import argparse
 parser = argparse.ArgumentParser()
-parser.add_argument("echo")
+parser.add_argument("square", help="display a square of a given number",
+                    type=int)
 args = parser.parse_args()
-print(args.echo)
+print(args.square**2)
